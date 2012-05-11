208fe5b162a58144b56c10aaf33d67374756ef3b
Björn <bjoern@aboalarm.de>
2012-05-11 09:57:18 +0200

    Added comments, removed some old code

--- Version.php
+++ Version.php
@@ -1,11 +1,13 @@
 <?php
 
 
-class Version extends Eloquent {
+class Version {
 
 	static $table = "versions";
-	static $timestamps = false;
 	
+	/**
+	* Save the current state of the object
+	**/
 	public static function freeze($src) {
 		$data = json_encode($src->original);
 		$table = strtolower(get_class($src));
@@ -13,16 +15,25 @@ class Version extends Eloquent {
 		return DB::table(Version::$table)->insert(array('data' => $data, 'object_table' => $table, 'object_id' => $obj_id));
 	}
 	
+	/**
+	* Load a saved version from the database
+	**/
 	public static function unfreeze($document_id) {
 		$data = DB::table(Version::$table)->where_id($document_id)->first();
 		$data->data = json_decode($data->data);
 		return new $data->object_table($data->data);
 	}
 	
+	/**
+	* Get all saved versions for an specific object
+	**/
 	public static function getFrozenObjects($object_id, $object_table) {
 		return DB::table(Version::$table)->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->get();
 	}
 	
+	/**
+	* Get the last saved version of an object
+	*/
 	public static function getLatestFreeze($object_id, $object_table) {
 		return DB::table(Version::$table)->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->order_by('created_at', 'desc')->first();
 	}
