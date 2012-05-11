7348288255e2e238078d265b59102b00fb8d6ea7
Björn <bjoern@aboalarm.de>
2012-05-11 10:49:09 +0200

    Changed how the parameters of the methods

--- README.md
+++ README.md
@@ -13,7 +13,7 @@ Usage
 $old_object = User::find(1);
 
 // Save a version
-if(Version::freeze($old_object)) {
+if(Version::save($old_object)) {
     echo 'Version saved.';
 }
 ?>
@@ -22,7 +22,9 @@ if(Version::freeze($old_object)) {
 **Load all versions of an object**
 ```
 <?php
-$all = Version::getFrozenObjects(1, 'user');
+// Retrieve an object from the database
+$old_object = User::find(1);
+$all = Version::getAllVersions($old_object);
 print_r($all);
 ?>
 ```
@@ -30,13 +32,15 @@ print_r($all);
 **Load only the last saved version of an object**
 ```
 <?php
-$last_version = Version::getLatestFreeze(1, 'user');
+// Retrieve an object from the database
+$old_object = User::find(1);
+$last_version = Version::getLastSavedVersion($old_object);
 ?>
 ```
 
 **Load a specific version of an object**
 ```
 <?php
-$oldest_version = Version::unfreeze(3);
+$oldest_version = Version::load(3);
 ?>
 ```
--- Version.php
+++ Version.php
@@ -8,7 +8,7 @@ class Version {
 	/**
 	* Save the current state of the object
 	**/
-	public static function freeze($src) {
+	public static function save($src) {
 		$cfg = Config::get('version');
 		$data = json_encode($src->original);
 		$table = strtolower(get_class($src));
@@ -20,8 +20,8 @@ class Version {
 	/**
 	* Load a saved version from the database
 	**/
-	public static function unfreeze($document_id) {
-		$data = DB::table(Version::getVersionsTable())->where_id($document_id)->first();
+	public static function load($version_id) {
+		$data = DB::table(Version::getVersionsTable())->where_id($version_id)->first();
 		$data->data = json_decode($data->data);
 		return new $data->object_table($data->data);
 	}
@@ -29,15 +29,15 @@ class Version {
 	/**
 	* Get all saved versions for an specific object
 	**/
-	public static function getFrozenObjects($object_id, $object_table) {
-		return DB::table(Version::getVersionsTable())->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->get();
+	public static function getAllVersions($obj) {
+		return DB::table(Version::getVersionsTable())->where('object_id', '=', $obj->attributes['id'])->where('object_table', '=', strtolower(get_class($obj)))->get();
 	}
 	
 	/**
 	* Get the last saved version of an object
 	*/
-	public static function getLatestFreeze($object_id, $object_table) {
-		return DB::table(Version::getVersionsTable())->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->order_by('created_at', 'desc')->first();
+	public static function getLastSavedVersion($obj) {
+		return DB::table(Version::getVersionsTable())->where('object_id', '=', $obj->attributes['id'])->where('object_table', '=', strtolower(get_class($obj)))->order_by('created_at', 'desc')->first();
 	}
 	
 	public static function getVersionsTable() {
