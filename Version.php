3fdef452fc92ac3360ad166ff84d36b838410c22
Björn <bjoern@aboalarm.de>
2012-05-11 10:20:32 +0200

    Added support for a config file

--- Version.php
+++ Version.php
@@ -9,10 +9,11 @@ class Version {
 	* Save the current state of the object
 	**/
 	public static function freeze($src) {
+		$cfg = Config::get('version');
 		$data = json_encode($src->original);
 		$table = strtolower(get_class($src));
 		$obj_id = isset($src->original['id']) ? $src->original['id'] : false;
-		if($obj_id) return DB::table(Version::$table)->insert(array('data' => $data, 'object_table' => $table, 'object_id' => $obj_id));
+		if($obj_id) return DB::table(Version::getVersionsTable())->insert(array('data' => $data, 'object_table' => $table, 'object_id' => $obj_id));
 		return false;
 	}
 	
@@ -20,7 +21,7 @@ class Version {
 	* Load a saved version from the database
 	**/
 	public static function unfreeze($document_id) {
-		$data = DB::table(Version::$table)->where_id($document_id)->first();
+		$data = DB::table(Version::getVersionsTable())->where_id($document_id)->first();
 		$data->data = json_decode($data->data);
 		return new $data->object_table($data->data);
 	}
@@ -29,14 +30,19 @@ class Version {
 	* Get all saved versions for an specific object
 	**/
 	public static function getFrozenObjects($object_id, $object_table) {
-		return DB::table(Version::$table)->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->get();
+		return DB::table(Version::getVersionsTable())->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->get();
 	}
 	
 	/**
 	* Get the last saved version of an object
 	*/
 	public static function getLatestFreeze($object_id, $object_table) {
-		return DB::table(Version::$table)->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->order_by('created_at', 'desc')->first();
+		return DB::table(Version::getVersionsTable())->where('object_id', '=', $object_id)->where('object_table', '=', $object_table)->order_by('created_at', 'desc')->first();
+	}
+	
+	public static function getVersionsTable() {
+		$cfg = Config::get('version');
+		return $cfg['table'];
 	}
 
 }
---
+++ config/version.php
@@ -0,0 +1,11 @@
+<?php
+
+/** 
+	Copy this file into your
+	application/config directory
+**/
+
+return array('table' => 'versions');
+
+
+?>
